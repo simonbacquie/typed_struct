9c529ebdf8b8ab492f668344c27b2f074f531a4d
Simon Bacquie <simonblah@gmail.com>
2015-09-19 13:21:28 -0400

    First commit

---
+++ test/minitest/typed_struct_spec.rb
@@ -0,0 +1,43 @@
+require_relative "../../typed_struct.rb"
+require "minitest/autorun"
+
+describe TypedStruct do
+
+  before do
+    # let(:metadata) { ImageMetadata.new(params) }
+    # @metadata = ImageMetadata.new(params);
+  end
+
+  # describe "missing required attributes" do
+  it "fails with ArgumentError when required attributes are missing" do
+    params = {pixel_width: 640, pixel_height: 480}
+    ->{ ImageMetadata.new(params) }.must_raise ArgumentError
+  end
+  # end
+
+  it "fails with TypeError when attributes have the wrong type" do
+    params = {title: "Test", taken_at: "2015-09-06"}
+    ->{ ImageMetadata.new(params) }.must_raise TypeError
+  end
+
+  it "has accessible attributes after validation passes" do
+    params = {title: "Test", taken_at: Time.now}
+    struct = ImageMetadata.new(params)
+    struct.title.must_equal "Test"
+    struct.taken_at.wont_be_nil
+  end
+
+  it "has settable attributes after validation passes" do
+    params = {title: "Test", taken_at: Time.now}
+    struct = ImageMetadata.new(params)
+    struct.title = "Modified Title"
+    struct.title.must_equal "Modified Title"
+  end
+
+  it "ignores attributes passed that are not defined in the class" do
+    params = {title: "Test", taken_at: Time.now, bogus_attribute: 123}
+    struct = ImageMetadata.new(params)
+    ->{ struct.bogus_attribute }.must_raise NoMethodError
+  end
+
+end
---
+++ typed_struct.rb
@@ -0,0 +1,79 @@
+class TypedStruct
+
+  @@types = {}
+  @@required_attributes = []
+  @@defined_attributes = []
+
+  def initialize params
+    create_getters(params.keys)
+    create_setters(params.keys)
+    @type_errors = []
+    @requirement_errors = []
+
+    raise ArgumentError, @requirement_errors.join(',') unless valid_by_requirements?(params.keys)
+    raise TypeError, @type_errors.join(', ') unless valid_by_types?(params)
+
+    set_initial_values(params)
+  end
+
+  def create_getters attributes
+    attributes.each do |a|
+      self.instance_eval("def #{a};@attribute_#{a};end") if defined_attribute?(a)
+    end
+  end
+
+  def create_setters attributes
+    attributes.each do |a|
+      self.instance_eval("def #{a}=(new_value);@attribute_#{a}=new_value;end") if defined_attribute?(a)
+    end
+  end
+
+  def set_initial_values params
+    params.keys.each do |p|
+      instance_variable_set("@attribute_#{p}", params[p]) if defined_attribute?(p)
+    end
+  end
+
+  def self.attribute name, klass, options=nil
+    @@types[name] = klass
+    @@required_attributes << name if options.is_a?(Hash) && options[:required]
+    @@defined_attributes << name
+  end
+
+  def defined_attribute? attribute
+    @@defined_attributes.include? attribute
+  end
+
+  def valid_by_requirements? attributes
+    (@@required_attributes - attributes).each do |ra|
+      @requirement_errors << "Attribute '#{ra}' is required"
+    end
+
+    return false if @requirement_errors.length > 0
+    true
+  end
+
+  def valid_by_types? params
+    params.keys.each do |p|
+      if @@types[p] && @@types[p] != params[p].class
+        @type_errors << "Attribute '#{p}' must be of type #{@@types[p].to_s}"
+      end
+    end
+
+    return false if @type_errors.length > 0
+    true
+  end
+
+end
+
+
+class ImageMetadata < TypedStruct
+
+  attribute :title,        String, required: true
+  attribute :description,  String
+  attribute :pixel_width,  Fixnum
+  attribute :pixel_height, Fixnum
+  attribute :dpi,          Fixnum
+  attribute :taken_at,     Time,   required: true
+
+end
